6252eb34df2263d51ecd7f728b534d302bd0cb99
Keenan <kvenuti@gmail.com>
2019-04-05 16:02:36 -0700

    Add files via upload

--- vg_process.py
+++ vg_process.py
@@ -6,10 +6,11 @@ import time
 import scraperwiki
 import lxml.html
 import json
-from random import shuffle
 from os import listdir
 from os.path import isfile, join
-btch_size = 20
+#more comments, a bit more complex than my other scrape
+btch_size = 200
+attr_to_pull = 'img_link'
 
 #pulls a photo and convertsit to black and white
 def pull_bnw_photo(url):
@@ -18,10 +19,6 @@ def pull_bnw_photo(url):
 	np_img = np.array(img)/255
 	return np_img.tolist()
 
-#simple function to save dic as json file so many issues with the pickle
-def save_j(dic, nm='g'):
-	with open(nm+'.json', 'w') as fp:
-		json.dump(dic, fp)
 #pulls a games summary
 def pull_txt_sum(url):
 	html = scraperwiki.scrape(url)
@@ -35,17 +32,23 @@ def pull_txt_sum(url):
 			desc = desc.strip()
 			return desc
 	return ''
+
+#simple function to save dic as json file so many issues with the pickle
+def save_j(dic, nm='g', do='w'):
+	with open(nm+'.json', do) as fp:
+		json.dump(dic, fp)
+
 #these two are try except wrapper functions to not distribut flow
 def pull_pic(url):
 	try:
 		return pull_bnw_photo(url)
 	except:
 		return [[0,0],[0,0]]
-def pull_summary(url, title):
+def pull_summary(url):
 	try:
 		return pull_txt_sum(url)
 	except:
-		return title
+		return url
 
 #needed to batch these dictionaries as it was getting pretty large
 #produces a list of n dictionaries
@@ -59,12 +62,16 @@ def batch_dic(dic):
 	return ret_dics
 
 #pull names of every completed game so far, once I've run this script a couple times, I don't want to have to re run previously used games
+#takes a while so going to save an external list as well
 def pull_completed(path):
-	complete_game_files = [f for f in listdir(path) if isfile(join(path, f))]
+	file_nms = [f for f in listdir(path) if isfile(join(path, f))]
 	complete_games = []
-	for complete_game_file in complete_game_files:
-		complete_games.extend(list(complete_game_file))
-	return set(complete_games)
+	for file_nm in file_nms:
+		with open(path+file_nm, 'rb') as h:
+			complete_game_file = json.load(h)
+		complete_games.extend(list(complete_game_file.keys()))
+		print('Loaded completed game library %s'%path+file_nm)
+	return complete_games
 
 #run a dictionary, do the function we want to do and save it as a json file
 def run_and_save(start_dic, attr, attr_func, l_nm=0):
@@ -75,44 +82,52 @@ def run_and_save(start_dic, attr, attr_func, l_nm=0):
 
 #unlike other script, this uses batches as files were much larger and saving them all in one file/storing all at once in RAM was killing my pc
 #if batched games already exists, load that, otherwise, load in the original games json file, clean out the bad data, batch it, and save the list of batches
+#wanted to keep this the same for train tst splitting
 try:
 	with open('games_as_%s_batches.json' % btch_size, 'rb') as h:
 		batches = json.load(h)
-	print('Loaded %s game batches' & btch_size)
+	print('Loaded %s game batches' % btch_size)
 except:
 	#load all of our game data (doesn't have images or desc yet)
 	with open('org_games.json', 'rb') as h:
 		all_games = json.load(h)
 	#clean it to make sure we aren't scraping data we can't use in analysis
 	games = {}
-	for k, v in unloaded_games.items():
+	for k, v in all_games.items():
 		if v['sales'] != 'N/A' or v['shipped'] != 'N/A':
 			games[k] = v
 	#divide it up into batches
 	batches = batch_dic(games)
 	#save batches
 	save_j(batches, nm='games_as_%s_batches' % btch_size)
-	print('Wrote %s game batches' & btch_size)
+	print('Wrote %s game batches' % btch_size)
 
+#look for a completed games list, if it doesn't exist make one
+try:
+	with open(attr_to_pull+'/completed_games.json', 'rb') as f:
+		completed_games = json.load(f)
+	print('Loaded %s list of completed_games' % attr_to_pull)
+except:
+	#this takes a while with all that json so I wanted to try to shorten it
+	completed_games = pull_completed(attr_to_pull+'/')
+	save_j(completed_games, attr_to_pull+'/completed_games')
+	print('Wrote %s list of completed_games' % attr_to_pull)
+
+#feed in function based on what we're pulling
+func_dic = {'img_link':pull_pic, 'game_url':pull_summary}
 #some stuff I like to print out but don't want to load every time
 len_games = sum([len(batch) for batch in batches])
 len_bathces = len(batches)
 start_time = time.time()
 
-completed_game_imgs = pull_completed('img_link/')
-#completed_game_sums = pull_completed('game_url/')
-
 #go through each batch
 for dex, dic_seg in enumerate(batches):
 	#if we loaded the batches, want to make sure we don't re run games
-	if not set(dic_seg.keys()) < completed_game_imgs:
-		print("Batch#: %s of %s, Batch Size %s of %s total games - %s seconds" % (dex,len_bathces,len(dic_seg),len_games,round(time.time() - start_time,4)))
-		#run through a batches game image links, download and save
-		run_and_save(dic_seg, 'img_link', pull_pic, str(dex))
-
-#for dex, dic_seg in enumerate(batches):
-	#if we loaded the batches, want to make sure we don't re run games
-	#if not set(dic_seg.keys()) < completed_game_sums:
-	#	print("Batch#: %s of %s, Batch Size %s of %s total games - %s seconds" % (dex,len_bathces,len(dic_seg),len_games,round(time.time() - start_time,4)))
-		#run through summaries, download and save
-	#	run_and_save(dic_seg, 'game_url', pull_summary, str(dex))
+	if not set(dic_seg.keys()) < set(completed_games):
+		print("Batch #%s of %s, Batch Size %s of %s total games - %s seconds" % (dex,len_bathces,len(dic_seg),len_games,round(time.time() - start_time,4)))
+		#run through a batches game image links or game urls, download and save
+		run_and_save(dic_seg, attr_to_pull, func_dic[attr_to_pull], str(dex))
+		save_j(list(dic_seg.keys())+completed_games,attr_to_pull+'/completed_games')
+	else:
+		print("Batch #%s is already loaded in %s/" % (str(dex),attr_to_pull))
+		time.sleep(.001)
