9a9ac24379ee5cf1508bc79b79ea20c55c61452e
Keenan <kvenuti@gmail.com>
2019-04-04 18:20:09 -0700

    new updates

--- vg_process.py
+++ vg_process.py
@@ -8,12 +8,6 @@ import pickle
 import scraperwiki
 import lxml.html
 
-def confloat(value):
-	try:
-		return float(value)
-	except ValueError:
-		return 0
-
 def pull_bnw_photo(url):
 	response = requests.get(url)
 	img = Image.open(BytesIO(response.content)).convert('L')
@@ -24,7 +18,7 @@ def save_dic(dic, nm='g'):
 	with open(nm+'.pickle', 'wb') as handle:
 		pickle.dump(dic, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-def pull_summary(url):
+def pull_txt_sum(url):
 	html = scraperwiki.scrape(url)
 	root = lxml.html.fromstring(html)
 	gbbs = root.cssselect("div#gameBodyBox")
@@ -37,52 +31,28 @@ def pull_summary(url):
 			return desc
 	return ''
 
-class game():
-	def __init__(self,title,score,date,developer,url,rank,publisher,sales,shipped,user_score,vg_score):
-		self.title=title
-		self.score=score
-		self.date=date
-		self.developer=developer
-		self.url=url
-		self.rank=rank
-		self.publisher = publisher
-		sales_chk = max(confloat(sales), confloat(shipped))
-		self.sales = sales_chk if sales_chk != 0 else np.nan
-		self.user_score=user_score
-		self.vg_score=vg_score
-	def pull_pic(self, url):
-		try:
-			self.img_arr = pull_bnw_photo(url)
-		except:
-			self.img_arr = np.array([[0,0],[0,0]])
-	def pull_summary(self):
-		try:
-			self.summary = pull_summary(self.url)
-		except:
-			self.summary = self.title
+def pull_pic(url):
+	try:
+		img_arr = pull_bnw_photo(url)
+	except:
+		img_arr = np.array([[0,0],[0,0]])
+def pull_summary(url, title):
+	try:
+		summary = pull_txt_sum(url)
+	except:
+		summary = title
 
 with open('games.pickle', 'rb') as h:
 	games = pickle.load(h)
 
 #make game, scrape pics and summary
-game_objs = {}
-bads = []
+games_complete = games
 dex = 0
 all_games = len(games)
 start_time = time.time()
-for nm, ats in games.items():
-	if dex % 500 ==0:
-		print("Page#: %s of %s - %s seconds - Scraped:%s, Failed:%s" % (dex,all_games,round(time.time() - start_time,4),len(game_objs),len(bads)))
-		save_dic(game_objs,'game_objects')
-	try:
-		game_obj = game(nm,ats['critic_score'],ats['date'],ats['developer'],ats['game_url'],ats['pos'],ats['publisher'],
-					   ats['sales'].replace('m',''),ats['shipped'].replace('m',''),ats['user_score'],ats['vg_score'])
-		game_obj.pull_pic(ats['img_link'])
-		game_obj.pull_summary()
-		game_objs[nm] = game_obj
-	except Exception as e:
-		print(nm,' ',e)
-		bads.append(nm)
-	dex +=1
-	time.sleep(.0000001)
-save_dic('game_objects')
+for dex, (nm, ats) in enumerate(games.items()):
+	if dex % 100 ==0:
+		print("Page#: %s of %s - %s seconds" % (dex,all_games,round(time.time() - start_time,4)))
+		save_dic(games_complete,'games_complete')
+	games_complete[nm].update({'img_arr':pull_pic(ats['img_link']), 'desc':pull_summary(ats['game_url'], nm)})
+save_dic(games_complete,'games_complete')
